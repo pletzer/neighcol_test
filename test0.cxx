d9be7d6df78fc37ae354be8c73be63b4e94f4fcd
pletzera <pletzera@niwa-1007638.niwa.local>
2016-03-04 16:14:05 +1300

    + print out rank/size

--- test0.cxx
+++ test0.cxx
@@ -3,10 +3,15 @@
  */
 
 #include <mpi.h>
+#include <iostream>
 
 int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
-
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    std::cout << "[" << rank << "] out of 0..." 
+              << size - 1 << " procs\n"; 
     MPI_Finalize();
     return 0;
 }
